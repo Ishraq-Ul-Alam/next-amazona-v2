5e57d9e097324cc689c4cbd1be92f441ef6baa76
Ishraq-Ul-Alam <105166186+Ishraq-Ul-Alam@users.noreply.github.com>
2024-03-25 20:15:58 +1100

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
+import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from '@/components/header/header'
+import Providers from '@/components/Providers'
+
+import Sidebar from '@/components/Sidebar'
+import Header from '@/components/header/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Netx Amazona V2',
+  title: 'Next Amazona V2',
   description: 'Modern ECommerce Website',
 }
 
@@ -18,13 +21,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="min-h-screen flex flex-col">
-          <Header />
-          {children}
-          <footer className="footer footer-ccenter p-4 bg-baase-300 text-base-content">
-            <p> Copyright © 2023 - All right reserved by Next Amazona V2t</p>
-          </footer>
-        </div>
+        <Providers>
+          <div className="drawer">
+            <div className="drawer-content">
+              <div className="min-h-screen flex flex-col">
+                <Header />
+                {children}
+                <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                  <p>
+                    Copyright © 2023 - All right reserved by Next Amazona V2
+                  </p>
+                </footer>
+              </div>
+            </div>
+            <div className="drawer-side">
+              <label
+                htmlFor="my-drawer"
+                aria-label="close sidebar"
+                className="drawer-overlay"
+              ></label>
+              <Sidebar />
+            </div>
+          </div>
+        </Providers>
       </body>
     </html>
   )
